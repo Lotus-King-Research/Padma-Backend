0223733299b910cb2e2cb51ec039f0eb943c4a0e
Mikko Kotila <mikko@pop-os.localdomain>
2020-07-23 21:46:05 +0300

    fully image based

--- server/setup.sh
+++ server/setup.sh
@@ -1,5 +1,3 @@
-read IP
-
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
@@ -24,9 +22,35 @@ unzip -qq -o /tmp/docs.zip -d /tmp/docs/
 wget -qq --show-progress https://github.com/mikkokotila/Rinchen-Terdzo-Tokenized/raw/master/tokens/tokens.zip -O /tmp/tokens.zip
 unzip -qq -o /tmp/tokens.zip -d /tmp/tokens
 
-sudo cp ./server/Padma.service /etc/systemd/system/Padma.service
+sudo systemctl nginx stop
 sudo sed "s/_IP_/$IP/" server/nginx.conf > /etc/nginx/nginx.conf
+sudo systemctl nginx start
 
-sudo systemctl daemon-reload
-sudo systemctl start Padma
-sudo systemctl status Padma
+# install docker
+sudo apt-get update -y
+
+sudo apt-get install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo docker login docker.pkg.github.com --username mikkokotila --password ${{ secrets.MIKKOKOTILA_TOKEN }}
+sudo docker pull docker.pkg.github.com/mikkokotila/padma/core_api:master
+NEW_IMAGE_ID=$(sudo docker images | grep core_api | tail -1 | tr -s ' ' | cut -d ' ' -f3)
+sudo docker stop $CURRENT_IMAGE_ID
+sudo docker run --restart unless-stopped -p 5000:5000 $NEW_IMAGE_ID
+echo $NEW_IMAGE_ID > current_image_id.txt
