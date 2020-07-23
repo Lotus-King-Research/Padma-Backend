2a7a746acb6e24d470806c6d57acca47d1f828a4
Mikko Kotila <mikko@pop-os.localdomain>
2020-07-23 08:19:20 +0300

    missing changes

--- server/setup.sh
+++ server/setup.sh
@@ -15,6 +15,15 @@ cd Padma
 pip3 install -r requirements.txt
 python3 -m spacy download en
 
+wget -qq --show-progress https://goo.gl/GyTv7n -O /tmp/dictionaries.zip
+unzip -qq -o /tmp/dictionaries.zip -d /tmp
+
+wget -qq --show-progress https://github.com/mikkokotila/Rinchen-Terdzo-Tokenized/raw/master/docs/docs.zip -O /tmp/docs.zip
+unzip -qq -o /tmp/docs.zip -d /tmp/docs/
+
+wget -qq --show-progress https://github.com/mikkokotila/Rinchen-Terdzo-Tokenized/raw/master/tokens/tokens.zip -O /tmp/tokens.zip
+unzip -qq -o /tmp/tokens.zip -d /tmp/tokens
+
 sudo cp ./server/Padma.service /etc/systemd/system/Padma.service
 sudo sed "s/_IP_/$IP/" server/nginx.conf > /etc/nginx/nginx.conf
 
