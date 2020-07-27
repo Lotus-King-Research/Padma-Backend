a4189cc0e49e20f0df63152294808a870ddea201
Mikko Kotila <mikko@pop-os.localdomain>
2020-07-27 21:51:11 +0300

    load data from host

--- app/utils/initialize.py
+++ app/utils/initialize.py
@@ -4,18 +4,6 @@ def initialize():
 
     from .create_dictionary import create_dictionary
     from .create_texts import create_texts
-    
-    if os.path.isfile('/tmp/All_Dictionaries_report_2016.tab') is False:
-        os.system('wget -qq --show-progress https://goo.gl/GyTv7n -O /tmp/dictionaries.zip')
-        os.system('unzip -qq -o /tmp/dictionaries.zip -d /tmp')
-
-    if os.path.isdir('/tmp/docs') is False:
-        os.system('wget -qq --show-progress https://github.com/mikkokotila/Rinchen-Terdzo-Tokenized/raw/master/docs/docs.zip -O /tmp/docs.zip')
-        os.system('unzip -qq -o /tmp/docs.zip -d /tmp/docs/')
-
-    if os.path.isdir('/tmp/tokens') is False:
-        os.system('wget -qq --show-progress https://github.com/mikkokotila/Rinchen-Terdzo-Tokenized/raw/master/tokens/tokens.zip -O /tmp/tokens.zip')
-        os.system('unzip -qq -o /tmp/tokens.zip -d /tmp/tokens')
 
     dictionary = create_dictionary()
     texts = create_texts()
