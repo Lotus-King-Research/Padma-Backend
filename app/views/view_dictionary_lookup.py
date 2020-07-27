6df431f1010fd0c9f2a8c43e71194f1127cd91fa
Mikko Kotila <mikko@pop-os.localdomain>
2020-07-27 16:33:48 +0300

    remove views from package

--- app/views/view_dictionary_lookup.py
+++
@@ -1,9 +0,0 @@
-def view_dictionary_lookup(data):
-
-    from flask import render_template
-
-    return render_template('dictionary_lookup.html',
-                           query=data['search_query'],
-                           text=data['text'],
-                           source=data['source'],
-                           tokens=data['tokens'])
--- app/views/view_tokenize.py
+++
@@ -1,6 +0,0 @@
-def view_tokenize(data):
-
-    from flask import render_template
-
-    return render_template('tokenize.html',
-                           tokens=data['tokens'])
