c88ab0f42aa57fdeb0ed2e76b262f074fc151cfb
Mikko Kotila <mikko@pop-os.localdomain>
2020-07-28 21:56:59 +0300

    update tests

--- tests/deploy_tests.py
+++ tests/deploy_tests.py
@@ -1,12 +1,12 @@
 import requests
 from time import sleep
 
-end_points = ['http://api.padma.io/dictionary_lookup?query=པདྨ་འབྱུང་གནས་&mode=api',
-              'http://api.padma.io/search_texts?query=པདྨ་འབྱུང་གནས་&mode=api',
-              'http://api.padma.io/find_similar?query=པདྨ་འབྱུང་གནས་&mode=api',
-              'http://api.padma.io/word_statistics?query=པདྨ་&mode=api',
-              'http://api.padma.io/tokenize?query=པདྨ་འབྱུང་གནས་&mode=api',
-              'http://api.padma.io/render_text?title=Terdzo-ZI-052&start=2&end=4&mode=api']
+end_points = ['http://api.padma.io/dictionary_lookup?query=པདྨ་འབྱུང་གནས་',
+              'http://api.padma.io/search_texts?query=པདྨ་འབྱུང་གནས་',
+              'http://api.padma.io/find_similar?query=པདྨ་འབྱུང་གནས་',
+              'http://api.padma.io/word_statistics?query=པདྨ་',
+              'http://api.padma.io/tokenize?query=པདྨ་འབྱུང་གནས་',
+              'http://api.padma.io/render_text?title=Terdzo-ZI-052&start=2&end=4']
 
 for end_point in end_points:
 
