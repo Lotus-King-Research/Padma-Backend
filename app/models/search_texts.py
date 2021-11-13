6b70b94e251e486f575c926233b896a99aef7396
Mikko Kotila <upstairs@Mikkos-Mac-mini.local>
2021-11-13 13:47:20 +0200

    support for full text search

--- app/models/search_texts.py
+++ app/models/search_texts.py
@@ -18,7 +18,7 @@ def search_texts(request):
     if len(query) == 0:
         abort(404)
 
-    results = _search_texts_v2(query)
+    results = _search_texts_v3(query)
 
     if len(results) == 0:
         abort(404)
@@ -68,6 +68,50 @@ def _search_texts_v2(query):
 
     return out
 
+
+def _search_texts_v3(query):
+
+    from app import locations
+    from app import index
+    from app import texts
+
+    from app import tokenizer
+    from app.utils.tokenization import tokenization
+
+    out = []
+
+    # try to find the the whole input string
+    try:
+
+        index[query]
+        input_string = query
+        partial = False
+
+    # in case not found, get texts based on first token
+    except KeyError:
+        input_string = tokenization(query, tokenizer)[0]
+        partial = True
+
+    index_temp = index[input_string]
+
+    for filename_id in index_temp.keys():
+
+        filename = locations[filename_id].split('.')[0]
+        text_temp = texts(filename)
+        fragments = text_temp['text'].split('_')
+
+        for fragment_id in index_temp[filename_id]:
+            fragment = fragments[fragment_id]
+            
+            # handle the case where first syllable is use
+            if partial is True:
+                if query in fragment:
+                    out.append([fragment, filename, fragment_id, text_temp['text_title']])
+            # handle the default case
+            else:
+                out.append([fragment, filename, fragment_id, text_temp['text_title']])
+
+    return out    
 
 '''
 def _search_texts(query):
